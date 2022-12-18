502cffd4be044253dcabf0c271b7523da9ef1afb
Kamil Grondys <kamil.grondys@codecool.com>
2022-12-18 11:27:49 +0100

    adding POST support to the form

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/note")
+@app.route("/note", methods=["POST"])
 def add_note():
-    print("data from request: ", request.args.get["name"], request.args.get("note"))
-    return "OK"
+    #print("data from request: ", request.args.get["name"], request.args.get("note"))
+    print("data from request: ", request.form.get("name"), request.form.get("note"))
+    return "OK, " + request.form.get("name") + ". Dziękuję za Twoją notatkę o treści: " + request.form.get("note")
 
