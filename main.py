d44e0dcfe48a41803799b5f2e82bf9ac7913f7d4
Kamil Grondys <kamil.grondys@codecool.com>
2022-12-18 11:57:29 +0100

    addig POST and GET in the single route

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/note", methods=["POST"])
+@app.route("/note", methods=["POST", "GET"])
 def add_note():
-    print("data from request: ", request.form.get("name"), request.form.get("note"))
+    if request.method == "POST":
+        print("data from request: ", request.form.get("name"), request.form.get("note"))
+        return redirect("/thanks")
+    elif request.method == "GET":
+        return "This supose to be a form"
     return redirect("/thanks")
 
 @app.route("/thanks")
